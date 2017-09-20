bca51c092e9c0e4fb134a66813e2584926013bbc
metamn <bartus.csongor@gmail.com>
2017-09-20 11:07:44 +0300

    Homepage added

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="home">
+    <h3>Home</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
