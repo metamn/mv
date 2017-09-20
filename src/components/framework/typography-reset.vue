bca4674bc78940f7e3b975a029b5892e62bc9865
metamn <bartus.csongor@gmail.com>
2017-09-20 22:52:20 +0300

    CSS reset library changed to Normalize.css

--- src/components/framework/typography-reset.vue
+++ src/components/framework/typography-reset.vue
@@ -4,15 +4,43 @@
 
 <script>
   import 'normalize.css'
-  
+
   export default {
     name: 'mv-typography-reset'
   }
 </script>
 
 <style lang="scss">
-  // External CSS reset
-  // @import './../../assets/css/reset.css';
+  // Sitting on top of normalize.css
+  h1, h2, h3, h4, h5, h6 {
+    font-size: 1em;
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  p, figure, blockquote, ul, ol, li {
+    margin: 0;
+    padding: 0;
+  }
+
+  // Remove the extra bottom spacing for <img> inside <a>
+  a img {
+    line-height: 0;
+    display: block;
+  }
+
+  figure a {
+    display: block;
+  }
+
+  button,
+  input[type="submit"] {
+    border: none;
+    margin: 0;
+    padding: 0;
+    background-color: transparent;
+  }
 
   // Border box
   * {
