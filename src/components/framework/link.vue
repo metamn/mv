cc293458da73d83923572300f091756a90e92394
metamn <bartus.csongor@gmail.com>
2017-09-21 11:55:09 +0300

    Link component added

---
+++ src/components/framework/link.vue
@@ -0,0 +1,94 @@
+<template lang="html">
+  <a :class="computedClass" :href="computedURL" :title="title"><slot></slot></a>
+</template>
+
+<script>
+  export default {
+    name: 'mv-link',
+    props: {
+      /**
+       * The URL where the link points
+       */
+      url: {
+        type: String,
+        default: 'http://example.com'
+      },
+      /**
+       * The title of the link displayed on hover
+       */
+      title: {
+        type: String,
+        default: 'Link title'
+      },
+      /**
+       * The type of the url like email, telephone, etc.
+       */
+      type: {
+        type: String,
+        default: ''
+      },
+      /**
+       * The style of the url.
+       */
+      klass: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      /**
+       * Add a prefix to the URL if necessary
+       *
+       * Like 'mailto:' or 'tel:'
+       *
+       * @public
+       * @param {String} type The type of the URL
+       * @param {String} url The value of the URL
+       * @return {String} the URL or the URL with a prefix
+       */
+      computedURL () {
+        switch (this.type) {
+          case 'email':
+          case 'e-mail':
+            return 'mailto:' + this.url
+          case 'phone':
+          case 'tel':
+            return 'tel:' + this.url
+          default:
+            return this.url
+        }
+      },
+
+      /**
+       * Style the component
+       *
+       * This seems to be not working, I mean the return is lost somehowin Vue
+       * If I add manually the '$style.default' to :class it works
+       *
+       * @public
+       * @param {String} klass The styles to be added
+       * @return {computedClass} a CSS Module like computed stuff
+       */
+      computedClass () {
+        switch (this.klass) {
+          case 'underlined':
+            return this.$style.underlined
+          default:
+            return this.$style.default
+        }
+      }
+    }
+  }
+</script>
+
+<style module>
+  .default {
+    composes: link from './../framework/text-style.css';
+    text-decoration: none;
+  }
+
+  .underlined {
+    composes: default;
+    text-decoration: underline;
+  }
+</style>
--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -1,16 +1,20 @@
 <template>
   <section id="home" :class="[$style.defaultStyle]">
-    <mv-title type="section">Vue components for metamn</mv-title>
+    <mv-title type="section">
+      Vue components for <mv-link url="http://metamn.io" title="metamn" klass="underlined">metamn</mv-link>
+    </mv-title>
   </section>
 </template>
 
 <script>
   import title from './../project/title'
+  import link from './../framework/link'
 
   export default {
     name: 'home',
     components: {
-      'mv-title': title
+      'mv-title': title,
+      'mv-link': link
     }
   }
 </script>
