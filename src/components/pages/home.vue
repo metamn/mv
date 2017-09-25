b1abc43e6b33affcc7c5e7bea46522bcbb01ba7f
metamn <bartus.csongor@gmail.com>
2017-09-25 15:38:16 +0300

    Working on the about

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -2,18 +2,21 @@
   <section id="home" :class="[$style.defaultStyle]">
     <mv-strapline></mv-strapline>
     <mv-thumbnails></mv-thumbnails>
+    <mv-about></mv-about>
   </section>
 </template>
 
 <script>
   import strapline from './../project/strapline'
   import thumbnails from './../project/thumbnails'
+  import about from './../project/about'
 
   export default {
     name: 'home',
     components: {
       'mv-strapline': strapline,
-      'mv-thumbnails': thumbnails
+      'mv-thumbnails': thumbnails,
+      'mv-about': about
     }
   }
 </script>
---
+++ src/components/project/about.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <aside class="about">
+    <h3 hidden>About</h3>
+
+    <div class="text">
+      Viola Lukács is a Geneva-based curator, researcher and art professional. She has curated exhibitions at distinguished venues throughout Europe, including the Plutschow Gallery in Zürich, Pavillion Sicli in Geneva, and took part in the Ideal State Project at the dOCUMENTA(13) in Germany; she also recently curated MetaMetria, an acclaimed solo exhibition featuring the work of world-renowned sculpture artist Bernar Venet. Recently, she has been focusing on the question; what are the visual and dialectical tools of representation omitting seduction and fascination in our contemporary culture? Her interest quickened in performance and living arts, as well as in videos of the digital era.
+    </div>
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: 'mv-about'
+  }
+</script>
+
+<style module scoped>
+  div {
+    font-size: 1.77em;
+  }
+</style>
