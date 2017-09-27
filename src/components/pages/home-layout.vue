e2aa74b6f59f0295ebe9d18d21a35c5ad1b29dd8
metamn <bartus.csongor@gmail.com>
2017-09-27 19:45:06 +0300

    Infinite scroll works

--- src/components/pages/home-layout.vue
+++ src/components/pages/home-layout.vue
@@ -15,30 +15,56 @@
     components: {
       'mv-home': home
     },
+    data: function () {
+      return {
+        height: 0
+      }
+    },
+    mounted: function () {
+      const body = document.body
+      const html = document.documentElement
+      this.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+      console.log('h: ' + this.height)
+    },
     methods: {
+      getHeight () {
+        const body = document.body
+        const html = document.documentElement
+        return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+      },
+      createNewContainerDiv () {
+        const newNode = document.createElement('div')
+        newNode.classList.add('new-home-container')
+        return newNode
+      },
+      createNewContainer (direction) {
+        const elementID = (direction === 'up') ? '.vue-waypoint__waypoint:first-of-type' : '.vue-waypoint__waypoint:last-of-type'
+        const waypoint = document.querySelector(elementID)
+        const whereToInsert = (direction === 'up') ? waypoint.nextSibling : waypoint.previousSibling
+        const whatToInsert = this.createNewContainerDiv()
+        waypoint.parentNode.insertBefore(whatToInsert, whereToInsert)
+      },
+      addNewHome () {
+        const HomeCtor = Vue.extend({})
+        new HomeCtor({
+          render: function (createElement) {
+            return createElement(home)
+          }
+        }).$mount('.new-home-container')
+      },
       waypointTop (direction, going) {
         if ((direction.y === 'up') && going === 'in') {
-          var body = document.body
-          var html = document.documentElement
-          var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
           console.log('top')
-          const waypointTop = document.querySelector('.vue-waypoint__waypoint:first-of-type')
-          const newNode = document.createElement('div')
-          newNode.classList.add('new-home-container')
-          console.log(newNode)
-          waypointTop.parentNode.insertBefore(newNode, waypointTop.nextSibling)
-          const HomeCtor = Vue.extend({})
-          new HomeCtor({
-            render: function (createElement) {
-              return createElement(home)
-            }
-          }).$mount('.new-home-container')
-          window.scrollTo(0, height - 300)
+          this.createNewContainer(direction.y)
+          this.addNewHome()
+          window.scrollTo(0, 6126)
         }
       },
       waypointBottom (direction, going) {
         if ((direction.y === 'down') && going === 'in') {
-          console.log('bottom')
+          // console.log('bottom')
+          this.createNewContainer(direction.y)
+          this.addNewHome()
         }
       }
     }
