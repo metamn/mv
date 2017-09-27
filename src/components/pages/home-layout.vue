47d8e9c9d8e21873292bb98b1c45f8a3a5c0e7d7
metamn <bartus.csongor@gmail.com>
2017-09-27 19:55:18 +0300

    Adding docs

--- src/components/pages/home-layout.vue
+++ src/components/pages/home-layout.vue
@@ -27,16 +27,30 @@
       console.log('h: ' + this.height)
     },
     methods: {
+      /**
+       * Get the height of the home
+       * - then we can scroll down to the second home when a new one is inserted above
+       * - the problem is lazy loading: we don't know the real size of the home
+       * until we didn't scroolled down to the bottom first
+       */
       getHeight () {
         const body = document.body
         const html = document.documentElement
         return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
       },
+      /**
+       * Create a new container div where a new home will be added
+       */
       createNewContainerDiv () {
         const newNode = document.createElement('div')
         newNode.classList.add('new-home-container')
         return newNode
       },
+      /**
+       * Insert the new container where is appropiate
+       * @param  {[type]} direction [description]
+       * @return {[type]}           [description]
+       */
       createNewContainer (direction) {
         const elementID = (direction === 'up') ? '.vue-waypoint__waypoint:first-of-type' : '.vue-waypoint__waypoint:last-of-type'
         const waypoint = document.querySelector(elementID)
@@ -44,6 +58,9 @@
         const whatToInsert = this.createNewContainerDiv()
         waypoint.parentNode.insertBefore(whatToInsert, whereToInsert)
       },
+      /**
+       * Re-render the home component and mount where the new container is
+       */
       addNewHome () {
         const HomeCtor = Vue.extend({})
         new HomeCtor({
@@ -52,17 +69,27 @@
           }
         }).$mount('.new-home-container')
       },
+      /**
+       * The waypointTop event handler
+       * @param  {[type]} direction [description]
+       * @param  {[type]} going     [description]
+       * @return {[type]}           [description]
+       */
       waypointTop (direction, going) {
         if ((direction.y === 'up') && going === 'in') {
-          console.log('top')
           this.createNewContainer(direction.y)
           this.addNewHome()
-          window.scrollTo(0, 6126)
+          window.scrollTo(0, this.height)
         }
       },
+      /**
+       * The waypointBottom event handler
+       * @param  {[type]} direction [description]
+       * @param  {[type]} going     [description]
+       * @return {[type]}           [description]
+       */
       waypointBottom (direction, going) {
         if ((direction.y === 'down') && going === 'in') {
-          // console.log('bottom')
           this.createNewContainer(direction.y)
           this.addNewHome()
         }
