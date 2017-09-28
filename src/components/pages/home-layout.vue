a1142406b6f8b53e4bd2ce4df257fa6e47b0cdda
metamn <bartus.csongor@gmail.com>
2017-09-28 09:50:33 +0300

    Scroll works without lazy loading

--- src/components/pages/home-layout.vue
+++ src/components/pages/home-layout.vue
@@ -58,7 +58,6 @@
        */
       saveCurrentPosition () {
         this.previousScrollHeightMinusTop = document.documentElement.scrollHeight - document.documentElement.scrollTop
-        console.log(this.previousScrollHeightMinusTop)
       },
       restoreCurrentPosition () {
         document.documentElement.scrollTop = document.documentElement.scrollHeight - this.previousScrollHeightMinusTop
