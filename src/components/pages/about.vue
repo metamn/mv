c4351f7958296545db951c7dcdab4197c7742105
metamn <bartus.csongor@gmail.com>
2017-10-02 18:07:58 +0300

    Working on the About page

---
+++ src/components/pages/about.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <section>
+    <h3 hidden>About</h3>
+    <mv-about></mv-about>
+  </section>
+</template>
+
+<script>
+  import about from './../project/about'
+
+  export default {
+    name: 'about',
+    components: {
+      'mv-about': about
+    }
+  }
+</script>
+
+<style lang="scss">
+  .logo {
+    visibility: visible;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import homeLayout from '@/components/pages/home-layout'
+import about from '@/components/pages/about'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: homeLayout
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: about
     }
   ]
 })
