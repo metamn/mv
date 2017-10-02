ad5bd8d1411bfc4e0fd934860117bc385ac2ca45
metamn <bartus.csongor@gmail.com>
2017-10-02 18:26:49 +0300

    Working on the About page

--- src/components/project/about.vue
+++ src/components/project/about.vue
@@ -2,9 +2,11 @@
   <aside :class="$style.about">
     <h3 hidden>About</h3>
 
-    <div :class="$style.text">
-      Viola Lukács is a Geneva-based curator, researcher and art professional. She has curated exhibitions at distinguished venues throughout Europe, including the Plutschow Gallery in Zürich, Pavillion Sicli in Geneva, and took part in the Ideal State Project at the dOCUMENTA(13) in Germany; she also recently curated MetaMetria, an acclaimed solo exhibition featuring the work of world-renowned sculpture artist Bernar Venet. Recently, she has been focusing on the question; what are the visual and dialectical tools of representation omitting seduction and fascination in our contemporary culture? Her interest quickened in performance and living arts, as well as in videos of the digital era.
-    </div>
+    <router-link :class="$style.link" v-on:click.native="doSomethingCool" to="/about">
+      <div :class="$style.text">
+        Viola Lukács is a Geneva-based curator, researcher and art professional. She has curated exhibitions at distinguished venues throughout Europe, including the Plutschow Gallery in Zürich, Pavillion Sicli in Geneva, and took part in the Ideal State Project at the dOCUMENTA(13) in Germany; she also recently curated MetaMetria, an acclaimed solo exhibition featuring the work of world-renowned sculpture artist Bernar Venet. Recently, she has been focusing on the question; what are the visual and dialectical tools of representation omitting seduction and fascination in our contemporary culture? Her interest quickened in performance and living arts, as well as in videos of the digital era.
+      </div>
+    </router-link>
   </aside>
 </template>
 
@@ -16,6 +18,11 @@
 
 <style module>
   @value lem from './../framework/scale.css';
+
+  .link {
+    text-decoration: none;
+    color: gray;
+  }
 
   .about {
     background: linear-gradient(to right bottom, white, black);
